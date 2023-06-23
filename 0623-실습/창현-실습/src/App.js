c0b52ab13295198119a68fbc9741f4cd18ffddcd
gowso22 <gowso89@naver.com>
2023-06-23 15:48:31 +0900

    창현_실습1_테스트

---
+++ 0623-실습/창현-실습/src/App.js
@@ -0,0 +1,11 @@
+import Practice01 from "./Practice01";
+
+function App() {
+  return (
+    <div className="App">
+      <Practice01/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0623-실습/창현-실습/src/Practice02.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+
+
+function Practice02(){
+   
+
+
+    return(
+        <div>
+           
+        </div>
+    );
+
+}
+
+export default Practice02;
