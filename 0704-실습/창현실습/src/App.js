8ea5369f8b5ff208c75a0b0ab461acede08c388e
gowso22 <gowso89@naver.com>
2023-07-04 18:15:04 +0900

    약간수정

--- 0704-실습/창현실습/src/App.js
+++ 0704-실습/창현실습/src/App.js
@@ -1,10 +1,15 @@
-
+import Counter from './components/Counter';
+import Auth from './components/Auth'
+import Todo from './components/Todo'
 import Cart from './components/Cart';
 
 
 function App() {
   return (
     <div>
+     <Counter/>
+     <Auth/>
+     <Todo/> 
      <Cart/>
     </div>
   );
