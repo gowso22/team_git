359d8d12cd92d799f142f57cad4ab18592b4de4a
gowso22 <gowso89@naver.com>
2023-07-06 15:43:14 +0900

    약간수정

--- ToyProject/src/components/cafeList.js
+++ ToyProject/src/components/cafeList.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import CafeInfoList from "./CafeInfoList";
+import CafeInfoList from './cafeInfoList'
 
 
 const CafeList = () => {
