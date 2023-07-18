027caf3ca9a16825a4d5a3b8fd49f96958ce9ba1
박재형 <wogud114@gmail.com>
2023-07-18 16:44:42 +0900

    re-commit

--- pain-killer/src/components/Login.tsx
+++ pain-killer/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import { ChangeEvent } from "react";
 
 function LoginTsx() {
@@ -7,39 +7,41 @@ function LoginTsx() {
 
   const [logIn, setLogin] = useState(false);
 
-  const userNameChange = (event: ChangeEvent<HTMLInputElement>) =>  {
+  const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
-  }
+  };
 
-  const pwdChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handlePwdChange = (event: ChangeEvent<HTMLInputElement>) => {
     setPwd(event.target.value);
-  }
+  };
 
-  const handleSubmit = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if(username === "admin" && pwd === "password"){
-      setLogin(true);  // success
+    if (username === "admin" && pwd === "password") {
+      setLogin(true); // success
     } else {
       setLogin(false); // failure
     }
-  }
+  };
 
   return (
     <React.Fragment>
       <div>
         <div>Point T</div>
         <h4>관리자 로그인 | 직원 로그인</h4>
-        <div>센터코드</div>
-        <input type="number"></input>
-        <div>아이디</div>
-        <input type="id"></input>
-        <div>비밀번호</div>
-        <input type="passcode"></input>
-    </div>
+        <form onSubmit={handleSubmit}>
+          <div>센터코드</div>
+          <input type="number" />
+          <div>아이디</div>
+          <input type="id" onChange={handleUsernameChange} />
+          <div>비밀번호</div>
+          <input type="password" onChange={handlePwdChange} />
+          <button type="submit">로그인</button>
+        </form>
+      </div>
     </React.Fragment>
-  )
-
+  );
 }
 
 export default LoginTsx;
