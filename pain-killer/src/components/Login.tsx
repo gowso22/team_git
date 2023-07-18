6de9b20f0f0ac319aa3210c4063e3982d62c26d8
박재형 <wogud114@gmail.com>
2023-07-18 15:09:27 +0900

    more

--- pain-killer/src/components/Login.tsx
+++ pain-killer/src/components/Login.tsx
@@ -1,4 +1,5 @@
 import React , { useState } from "react";
+import { ChangeEvent } from "react";
 
 function LoginTsx() {
   const [username, setUsername] = useState("");
@@ -6,6 +7,35 @@ function LoginTsx() {
 
   const [logIn, setLogin] = useState(false);
 
-  
+  const userNameChange = (event: ChangeEvent<HTMLInputElement>) =>  {
+    setUsername(event.target.value);
+  }
+
+  const pwdChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPwd(event.target.value);
+  }
+
+  const handleSubmit = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    if(username === "admin" && pwd === "password"){
+      setLogin(true);  // success
+    } else {
+      setLogin(false); // failure
+    }
+  }
+
+  return (
+    <div>
+      <div>Point T</div>
+      <h4>관리자 로그인 | 직원 로그인</h4>
+      <div>센터코드</div>
+      <input type="number"></input>
+      <div>아이디</div>
+      <input type="id"></input>
+      <div>비밀번호</div>
+      <input type="passcode"></input>
+    </div>
+  )
 
 }
