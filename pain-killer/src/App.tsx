6e1bdb098e4bc25a57add15a59d0f69011058ff1
gowso22 <gowso89@naver.com>
2023-07-18 11:53:42 +0900

    update 이미지저장

--- pain-killer/src/App.tsx
+++ pain-killer/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Test from './Test'
+import Update from './components/Update'
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
      
-      <Test/>
+      <Update/>
     </>
   )
 }
