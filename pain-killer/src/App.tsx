c5a504d3cb905ce884724dab6de1a56abff3269c
Kim-Mingyeong <kalsrud0412@gmail.com>
2023-07-18 17:10:56 +0900

    Add Home

--- pain-killer/src/App.tsx
+++ pain-killer/src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Test from './Test'
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+// import Test from './Test';
+import Home from './pages/home/home';
+import Mypage from './pages/mypage/mypage';
 
 function App() {
-
   return (
     <>
-     
-      <Test/>
+      <Home />
+      <Mypage />
+      {/* <Test /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
