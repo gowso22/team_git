dabd711e8725587d2d98640aa1ddd951d8b1fddc
박재형 <wogud114@gmail.com>
2023-07-18 11:29:19 +0900

    Login tsx has been committed

---
+++ pain-killer/src/components/Login.tsx
@@ -0,0 +1,9 @@
+import React , { useState } from "react";
+
+function LoginTsx() {
+  const [username, setUsername] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const [logIn, setLogin] = useState("");
+
+}
