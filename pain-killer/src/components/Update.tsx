c3ccecf02b57e1bd2d40c504726b66834177701e
gowso22 <gowso89@naver.com>
2023-07-18 14:25:55 +0900

    업데이트화면

--- pain-killer/src/components/Update.tsx
+++ pain-killer/src/components/Update.tsx
@@ -5,10 +5,13 @@ const Update = () => {
 
     return(
         <div className="flex justify-center align-center flex-col">
-            <p className="mb-4">포인티 업데이트 알림</p>
+            <p className="mb-4 text-2xl font-bold">포인티 업데이트 알림</p>
             <p>포인티 최신 버전이 등록되었습니다.</p>
             <p>최신 버전으로 업데이트하시겠습니까?</p>
-            <img src ={updateAlarm} className='w-18 h-18'></img>
+            <img src ={updateAlarm}></img>
+
+            <p>*Wi-Fi가 아닐 경우 데이터 요금이 발생할 수 있습니다.</p>
+            <button className='bg-Pri-500 text-white px-3 py-4 text-xl '>업데이트</button>
         </div>
     )
 }
