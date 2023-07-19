8b9ba0597f870ecbd781ad16161f3b2b344e1076
박재형 <wogud114@gmail.com>
2023-07-19 18:43:55 +0900

    add ticket page , memberManage and revise App.tsx for faults

--- pain-killer/src/App.tsx
+++ pain-killer/src/App.tsx
@@ -2,18 +2,20 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import './App.css'
 import LoginTsx from "./components/Login";
-import EmployeeReg from './pages/Employee_registration/Employee_reg'
+/* import EmployeeReg from './pages/Employee_registration/Employee_reg'
 import InviteReg from './pages/Employee_registration/Employee_invite'
 import RegModal from './pages/Employee_registration/Employee_modal'
-import RegModal3 from './pages/Employee_registration/Employee_modal3.jsx'
-import LoginPage from './pages/Manager_login/login.jsx'
-import ContactVerification from './pages/Manager_login/ContactVerification.jsx'
+import RegModal3 from './pages/Employee_registration/Employee_modal3.jsx' */
+/* import LoginPage from './pages/Manager_login/login.jsx' */
+/* import ContactVerification from './pages/Manager_login/ContactVerification.jsx' */
 import ChangePassword from "./components/changePassword.js";
 import SimpleLoginPage from "./pages/simple_login/simpleLogin.js";
 import UserDetailPage from "./pages/simple_login/userDetail.js";
+import MemberManagementPage from "./pages/Member_Management/memberManage.js";
+import TicketPage from "./pages/Member_Management/ticket_page.js";
 // import ContactModal from './pages/Manager_login/ContactModal'
-import FindId from './pages/Manager_login/ContactIdCheck'
-import PwReset from './pages/Manager_login/ContactPwReset'
+/* import FindId from './pages/Manager_login/ContactIdCheck'
+import PwReset from './pages/Manager_login/ContactPwReset' */
 
 function App() {
 
@@ -25,17 +27,17 @@ function App() {
   
     <Router>
       <Routes>
-        <Route path="/" element={<EmployeeReg/>}></Route> {/* 직원등록 */}
-        <Route path="/invite" element={<InviteReg/>}></Route> {/* 직원초대 */}
-        <Route path="/modal" element={<RegModal/>}></Route> {/* 직원등록 다이얼로그 */}
-        <Route path="/modal2" element={<RegModal3/>}></Route> {/* 직원등록 다이얼로그3 */}
+        {/* <Route path="/" element={<EmployeeReg/>}></Route> */} {/* 직원등록 */}
+        {/* <Route path="/invite" element={<InviteReg/>}></Route> */} {/* 직원초대 */}
+        {/* <Route path="/modal" element={<RegModal/>}></Route> */} {/* 직원등록 다이얼로그 */}
+        {/* <Route path="/modal2" element={<RegModal3/>}></Route> */} {/* 직원등록 다이얼로그3 */}
 
         {/* 관리자 로그인 */}
 
-        <Route path="/managerlogin" element={<LoginPage/>}></Route> {/* 관리자 로그인 */}
-        <Route path="/contact" element={<ContactVerification/>}></Route> {/* 관리자 아이디비번찾기,연락처 인증 */}
-        <Route path="/findid" element={<FindId/>}></Route> {/* 관리자 아이디 찾기*/}
-        <Route path="/pwreset" element={<PwReset/>}></Route> {/* 관리자 비밀번호 재설정 */}
+        {/* <Route path="/managerlogin" element={<LoginPage/>}></Route> */} {/* 관리자 로그인 */}
+        {/* <Route path="/contact" element={<ContactVerification/>}></Route> */} {/* 관리자 아이디비번찾기,연락처 인증 */}
+        {/* <Route path="/findid" element={<FindId/>}></Route> */} {/* 관리자 아이디 찾기*/}
+        {/* <Route path="/pwreset" element={<PwReset/>}></Route> */} {/* 관리자 비밀번호 재설정 */}
 
         
 
@@ -47,11 +49,17 @@ function App() {
         {/* 비밀번호변경 페이지 */}
         <Route path="/change" element={<ChangePassword />} />
 
-       {/*  간편로그인 페이지 */}
-       <Route path="/simple" element={<SimpleLoginPage />} />
+        {/*  간편로그인 페이지 */}
+        <Route path="/simple" element={<SimpleLoginPage />} />
 
-       {/* 간편로그인 후 키패드 호출 페이지 */}
-       <Route path="/udetail" element={<UserDetailPage />} />
+        {/* 간편로그인 후 키패드 호출 페이지 */}
+        <Route path="/udetail" element={<UserDetailPage />} />
+
+        {/* 회원관리 */}
+        <Route path="/member" element={<MemberManagementPage />} />
+
+        {/* 수강권관리 */}
+        <Route path="/ticket" element={<TicketPage />} />
         
       </Routes>
     </Router>
