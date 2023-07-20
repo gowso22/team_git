fcd63bb1b4203200ea8f2a1a585b28b26a35e50e
Kim-Mingyeong <kalsrud0412@gmail.com>
2023-07-20 18:12:04 +0900

    Add calendar

--- pain-killer/src/App.tsx
+++ pain-killer/src/App.tsx
@@ -5,12 +5,16 @@ import './App.css';
 // import Test from './Test';
 import Home from './pages/home/home';
 import Mypage from './pages/mypage/mypage';
+import CalendarManager from './pages/schedule/calendar-manager';
+import CalendarEmployee from './pages/schedule/calendar-employee';
 
 function App() {
   return (
     <>
       {/* <Home /> */}
-      <Mypage />
+      {/* <Mypage /> */}
+      <CalendarManager />
+      {/* <CalendarEmployee /> */}
       {/* <Test /> */}
     </>
   );
