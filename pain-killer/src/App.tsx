321e73a816eadbb840f103ef5b9c24c1287200f0
Kim-Mingyeong <kalsrud0412@gmail.com>
2023-07-21 14:53:34 +0900

    Add weekly calendar

--- pain-killer/src/App.tsx
+++ pain-killer/src/App.tsx
@@ -1,43 +1,42 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import LoginPage from './pages/Manager_login/login';
+import Home from './pages/home/home';
+import Layout from './components/layout/Layout';
+import CalendarManager from './pages/schedule/calendar-manager';
+//import CalendarEmployee from './pages/schedule/calendar-employee';
+import MemberManage from './pages/member/MemberManage';
+import Mypage from './pages/mypage/mypage';
+import CenterManage from './pages/center/CenterManage';
 
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import './App.css'
-import LoginPage from "./pages/Manager_login/login";
-import Home from "./pages/home/home";
-import Layout from "./components/layout/Layout";
-import CalendarManager from "./pages/schedule/calendar-manager";
-import MemberManage from "./pages/member/MemberManage";
-import Mypage from "./pages/mypage/mypage";
-import CenterManage from "./pages/center/CenterManage";
-
+import WeekCalendar from './components/week-calendar';
 
 function App() {
-
   return (
     // 계속 불러올 컴포넌트는 components 폴더에
     // 페이지 내용에 해당하는 컴포넌트는 pages 폴더에
-    
+
     //  375px~400px
-  
+
     <div className="max-w-[396px] mx-auto relative">
-    <Router>
+      <Router>
+        <Routes>
+          {/*nav, footer 포함하는 컴포넌트는 여기 */}
+          <Route element={<Layout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/scheduleInfo" element={<CalendarManager />} />
+            <Route path="/memberInfo" element={<MemberManage />} />
+            <Route path="/centerInfo" element={<CenterManage />} />
+            <Route path="/myPage" element={<Mypage />} />
+          </Route>
 
-      <Routes> 
-        {/*nav, footer 포함하는 컴포넌트는 여기 */}
-        <Route element={<Layout/>}>
-          <Route path = "/home" element = {<Home/>}/>
-          <Route path = "/scheduleInfo" element={<CalendarManager/>}/>
-          <Route path = "/memberInfo" element={<MemberManage/>}/>
-          <Route path = "/centerInfo" element={<CenterManage/>}/>
-          <Route path = "/myPage" element={<Mypage/>}/>
-        </Route> 
-
-        {/*nav, footer 포함하지 않는 컴포넌트는 여기 */}
-        <Route path='/' element={<LoginPage/>}/>
-
-      </Routes>
-    </Router> 
-  </div>
-  )
+          {/*nav, footer 포함하지 않는 컴포넌트는 여기 */}
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/test" element={<WeekCalendar />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
