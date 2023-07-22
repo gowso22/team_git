a22ee29b48cbc570ce267b36459fc114c9026904
gowso22 <gowso89@naver.com>
2023-07-22 19:41:20 +0900

    직원전체조회_기능만_정리필요

--- pain-killer/src/pages/center/CenterManage.tsx
+++ pain-killer/src/pages/center/CenterManage.tsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from 'react';
 import Profile from '../../assets/Profile_24px.svg'
 import SearchBar from '../../components/search';
+import { Link } from 'react-router-dom';
+
+
+interface IEmpList{
+    id: number,
+    name: string,
+    phone: string,
+    memberCount: number,
+    rating: number,
+    memo: string
+}
+
 
 const CenterManage = () => {
+
+
+    const access_Token = localStorage.getItem('access_token');
+
+    const [empList, setEmpList] = useState<IEmpList[]>();
+
+    const getEmp = () => {
+        
+        try {
+      
+            fetch("http://223.130.161.221/api/v1/staffs", {
+              method: 'GET',
+              headers: {
+                "Authorization" : `Bearer ${access_Token}`,
+              },
+              
+            }).then((response) => response.json())
+              .then((result) => {
+                
+                console.log(result.meta);
+                console.log(result.datas);
+                console.log(result.message);
+
+                setEmpList(result.datas);
+                
+               }
+            )
+        } catch (error : any) {
+          alert(error);
+        }
+    }
+
+    useEffect(()=> {
+        getEmp()
+    }, [])
 
     return(
        <div className="flex flex-col items-center bg-[#F4F4F4] p-2 gap-3 h-[900px] overflow-y-auto">
@@ -17,23 +65,31 @@ const CenterManage = () => {
                     <button className="border rounded-[10px] py-1 px-[10px] text-[12px]">직원 등록</button>
                 </div>
             </div>
-            <div className="flex flex-col bg-[#FFFFFF] rounded-[4px] w-full px-[10px] py-3 gap-2">   
-                <div className="flex justify-between">
-                    <div className="flex gap-3">
-                        <img src={Profile} alt="프사"/>
-                        <span className="font-bold">김직원</span>
-                    </div>
-                    <span>010-0000-0000</span>
-                </div>
-                <div className="flex justify-between ">
-                    <span className="font-bold">총 회원수</span>
-                    <span>1000명</span>
-                </div>
-                <div className="flex justify-between ">
-                    <span className="font-bold">메모</span>
-                    <span>권한 변경 필요</span>
-                </div>
-            </div>
+           
+            { empList && 
+                empList.map((emp)=>(
+                    <Link to = "/">
+                        <div key={emp.id} 
+                             className="flex flex-col bg-[#FFFFFF] rounded-[4px] w-full px-[10px] py-3 gap-2">   
+                            <div className="flex justify-between">
+                                <div className="flex gap-3">
+                                    <img src={Profile} alt="프사"/>
+                                    <span className="font-bold">{emp.name}</span>
+                                </div>
+                                <span>{emp.phone}</span>
+                            </div>
+                            <div className="flex justify-between ">
+                                <span className="font-bold">총 회원수</span>
+                                <span>{emp.memberCount}명</span>
+                            </div>
+                            <div className="flex justify-between ">
+                                <span className="font-bold">메모</span>
+                                <span>{emp.memo}</span>
+                            </div>
+                        </div>
+                    </Link>
+                ))
+            }
        </div>
     )
 }
